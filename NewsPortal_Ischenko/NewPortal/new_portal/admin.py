d47eb409b14e3987e23a30bb5c19a607361045ea
Nastya <shamaevaanastasia98@gmail.com>
2024-07-30 18:23:17 +0300

    изменения после модуля D10

--- NewsPortal_Ischenko/NewPortal/new_portal/admin.py
+++ NewsPortal_Ischenko/NewPortal/new_portal/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Category, Post
 
+def nullfy_quantity(modeladmin, request, queryset): # все аргументы уже должны быть вам знакомы, самые нужные из них это request — объект хранящий информацию о запросе и queryset — грубо говоря набор объектов, которых мы выделили галочками.
+    queryset.update(quantity=0)
+nullfy_quantity.short_description = 'Обнулить товары' # описание для более понятного представления в админ панеле задаётся, как будто это объект
+
+# создаём новый класс для представления товаров в админке
+class PostAdmin(admin.ModelAdmin):
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    list_display = ('name', 'category', 'date_creation')
+    list_filter = ('date_creation', 'category', 'name')
+    search_fields = ('name', 'category__name')
 
 admin.site.register(Category)
 admin.site.register(Post)
